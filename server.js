6d8107ae682504fa7b9c0488e6c0fe6a0993dfd7
Collin <wildkard91@gmail.com>
2022-05-13 14:38:05 -0400

    created mutation hook for adding a new book, before only queries were possible

--- server.js
+++ server.js
@@ -102,8 +102,32 @@ const RootQueryType = new GraphQLObjectType ({
     })
 })
 
+const RootMutationType = new GraphQLObjectType({
+    name: "Mutation",
+    description: "Root Mutation",
+    fields: ()=>({
+        addBook: { type: BookType,
+        description: 'Add a Book',
+        args: {
+            name: {
+                type: GraphQLNonNull(GraphQLString)
+            },
+            authorId: {
+                type: GraphQLNonNull(GraphQLInt)
+            }
+        },
+        resolve: (parent, args) => {
+            const book = {id: books.length + 1, name: args.name, authorId: args.authorId}
+            books.push(book)
+            return book
+        }
+    }
+    })
+})
+
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 })
 
 app.use('/graphql', expressGraphQL({
